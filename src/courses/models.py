5ee213fddaf66b0e9614284a513c3249a331ed67
Saadat-Antor <md.sadat.amin@gmail.com>
2026-01-21 22:34:16 +0600

    created Courses model

---
+++ src/courses/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+
+
+class AccessRequirement(models.TextChoices):
+    ANYONE = 'publish', 'Published'
+    EMAIL_REQUIRED = 'email_required', 'Email Required'
+
+class PublishStatus(models.TextChoices):
+    PUBLISHED = 'publish', 'Published'
+    DRAFT = 'draft', 'Draft'
+    COMING_SOON = 'soon', 'Coming Soon'
+
+# COURSE Model
+class Course(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    status = models.CharField(max_length=10,
+                              choices=PublishStatus.choices,
+                              default=PublishStatus.DRAFT)
+    access = models.CharField(max_length=10,
+                              choices=AccessRequirement.choices,
+                              default=AccessRequirement.ANYONE)
+                                
+
+    @property
+    def is_published(self):
+        return self.status == PublishStatus.PUBLISHED
