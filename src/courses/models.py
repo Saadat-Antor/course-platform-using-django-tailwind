bb03cef1905c79eea1a5755a2d217e6d6ced32de
Saadat-Antor <md.sadat.amin@gmail.com>
2026-01-22 12:22:13 +0600

    replaced the image attribute of Course to a cloudinary image field

--- src/courses/models.py
+++ src/courses/models.py
@@ -1,5 +1,8 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
+import helpers
 
+helpers.cloudinary_init()
 
 
 class AccessRequirement(models.TextChoices):
@@ -22,7 +25,8 @@ class Course(models.Model):
     status = models.CharField(max_length=10,
                               choices=PublishStatus.choices,
                               default=PublishStatus.DRAFT)
-    image = models.ImageField(upload_to=handle_upload,blank=True, null=True)
+    # image = models.ImageField(upload_to=handle_upload,blank=True, null=True)
+    image = CloudinaryField("image", null=True)
     access = models.CharField(max_length=20,
                               choices=AccessRequirement.choices,
                               default=AccessRequirement.EMAIL_REQUIRED)
