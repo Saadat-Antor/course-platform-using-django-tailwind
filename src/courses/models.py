6cd145aa50b35514090a8db7d8a43334c77714f7
Saadat-Antor <md.sadat.amin@gmail.com>
2026-01-22 10:56:06 +0600

    updated the urls.py and registered Course to admin

--- src/courses/models.py
+++ src/courses/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 
 class AccessRequirement(models.TextChoices):
-    ANYONE = 'publish', 'Published'
+    ANYONE = 'anyone', 'Anyone'
     EMAIL_REQUIRED = 'email_required', 'Email Required'
 
 class PublishStatus(models.TextChoices):
