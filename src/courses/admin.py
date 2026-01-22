d4f658cf99b63181967af21b8e1478bb2864c814
Saadat-Antor <md.sadat.amin@gmail.com>
2026-01-22 15:43:44 +0600

    updated the Course & Lesson Model. And updated Admin panel.

--- src/courses/admin.py
+++ src/courses/admin.py
@@ -1,12 +1,18 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course, Lesson
 from django.utils.html import format_html
 from cloudinary import CloudinaryImage
 
 # Register your models here.
 
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra=0
+
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
+
     list_display = ["title", "status", "access"]
     list_filter = ["status", "access"]
 
--- src/courses/models.py
+++ src/courses/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
 import helpers
+from django.utils.text import slugify
 
 helpers.cloudinary_init()
 
@@ -18,6 +19,20 @@ class PublishStatus(models.TextChoices):
 def handle_upload(instance, filename):
     return f"{filename}"
 
+def get_public_id_prefix(instance, *args, **kwargs):
+    if instance.title:
+        slug = slugify(instance.title)
+        return f"courses/{slug}"
+    elif instance.id:
+        return f"courses/{instance.id}"
+    return "courses"
+
+def get_display_name(instance, *args, **kwargs):
+    if instance.title:
+        return instance.title
+    return "Course Upload"
+
+
 # COURSE Model
 class Course(models.Model):
     title = models.CharField(max_length=100)
@@ -26,10 +41,16 @@ class Course(models.Model):
                               choices=PublishStatus.choices,
                               default=PublishStatus.DRAFT)
     # image = models.ImageField(upload_to=handle_upload,blank=True, null=True)
-    image = CloudinaryField("image", null=True)
+    image = CloudinaryField("image", 
+                            null=True, 
+                            public_id_prefix=get_public_id_prefix, 
+                            tags=["course", "thumbnail"]
+                            )
     access = models.CharField(max_length=20,
                               choices=AccessRequirement.choices,
-                              default=AccessRequirement.EMAIL_REQUIRED)
+                              default=AccessRequirement.EMAIL_REQUIRED
+                              )
+    created = models.DateTimeField(auto_now_add=True)
                                 
 
     @property
@@ -63,7 +84,15 @@ class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
+    thumbnail = CloudinaryField("image", blank=True, null=True)
+    video = CloudinaryField("video", blank=True, null=True, resource_type="video")
+    order = models.IntegerField(default=0)
     can_preview = models.BooleanField(default=False, help_text="Can students view this lesson without enrolling?")
     status = models.CharField(max_length=10,
                               choices=PublishStatus.choices,
                               default=PublishStatus.PUBLISHED)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ['order', '-updated']
