65429f487fec5cd8dd0f3f028ce947c41493827b
Saadat-Antor <md.sadat.amin@gmail.com>
2026-01-24 22:51:08 +0600

    added verification email sending feature

--- src/emails/services.py
+++ src/emails/services.py
@@ -1,4 +1,6 @@
 from .models import Email, EmailVerificationEvent
+from django.core.mail import send_mail
+from django.conf import settings
 
 def verify_email(email):
     qs = Email.objects.filter(email=email, active=False)
@@ -6,15 +8,34 @@ def verify_email(email):
 
 def start_verification_event(email):
     email_obj, created = Email.objects.get_or_create(email=email)
-    obj = EmailVerificationEvent.objects.create(
+    verification_obj = EmailVerificationEvent.objects.create(
         parent=email_obj,
         email=email
     )
-    return obj
+    did_send = send_verification_email(verification_obj)
+    return verification_obj, did_send
+
+def send_verification_email(verification_obj):
+    text_msg: str|None = get_verification_email_msg(verification_obj, as_html=False)
+    text_html = get_verification_email_msg(verification_obj, as_html=True)
+
+    subject = "Please Verify Your Email - From Django"
+    from_email = settings.EMAIL_HOST_USER
+    to_email = verification_obj.email
+
+    return send_mail(
+        subject,
+        text_msg,
+        from_email,
+        [to_email],
+        fail_silently=False,
+        html_message=text_html
+    )
+
 
 def get_verification_email_msg(verification_instance, as_html=False):
     if not isinstance(verification_instance, EmailVerificationEvent):
-        return None
+        return ""
     if as_html:
         return f"<h1>{verification_instance.pk}</h1>"
     return f"{verification_instance.pk}"
